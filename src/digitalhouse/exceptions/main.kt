da08467c5839b1ebaf6a2eb2f3eb1dafa9a3a45d
Luiz Felipe Silva Braga <luiz@selectbrasil.com.br>
2020-09-21 21:11:36 -0300

    Perfumaria

--- src/digitalhouse/exceptions/main.kt
+++ src/digitalhouse/exceptions/main.kt
@@ -5,9 +5,15 @@ fun main() {
     val produto2 = Produto(5.0, "t2")
     val produtoComboSimples1 = ComboSimples(5.5, "c1")
     val produtoComboSimples2 = ComboSimples(10.0, "c2")
+    val produtoComboFamiliar = ComboFamiliar(12.0, "F1", mutableListOf(produtoComboSimples1, produtoComboSimples2))
 
-    val carrinho = Carrinho()
-    carrinho.adicionar(produto1, produto2, produtoComboSimples1, produtoComboSimples2)
+    val carrinho1 = Carrinho()
+    val carrinho2 = Carrinho()
+    carrinho1.adicionar(produto1, produto2, produtoComboSimples1, produtoComboSimples2)
+    carrinho2.adicionar(produtoComboFamiliar)
 
-    println(carrinho.getValorTotal())
+    println("Carrinho 1: ${carrinho1.getValorTotal()}")
+    carrinho1.produtos.forEach { println("  - $it") }
+    println("Carrinho 2: ${carrinho2.getValorTotal()}")
+    carrinho2.produtos.forEach { println("  - $it") }
 }
